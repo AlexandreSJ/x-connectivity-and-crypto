68cdf0ede4b1c424e66f2c46eef83958706ef2f8
wendellnd <wendelldourado.wn@gmail.com>
2023-09-03 20:11:03 -0300

    feat: adicionando mascara de cpf

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2';
+//import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,22 +7,66 @@ import Swal from 'sweetalert2';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  cpf_cnpj = '';
+  DECIMAL_SEPARATOR = '.';
+  GROUP_SEPARATOR = ',';
+  pureResult: any;
+  maskedId: any;
+  val: any;
+  v: any;
 
-  constructor() { 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  format(valString: string) {
+    if (!valString) {
+      return '';
+    }
+    let val = valString.toString();
+    const parts = this.unFormat(val).split(this.DECIMAL_SEPARATOR);
+    this.pureResult = parts;
+    if (parts[0].length <= 11) {
+      this.maskedId = this.cpf_mask(parts[0]);
+      return this.maskedId;
+    } else {
+      if (parts[0].length >= 14) {
+        return this.maskedId;
+      }
+
+      this.maskedId = this.cnpj(parts[0]);
+      return this.maskedId;
+    }
   }
 
-  ngOnInit() {
-  }
-  
-  fire(){
-    Swal.fire({
-      heightAuto: false,
-      title: 'Error!',
-      text: 'Do you want to continue',
-      icon: 'error',
-      confirmButtonText: 'Cool',
-      timer: 1500
-    })
+  unFormat(val: string) {
+    if (!val) {
+      return '';
+    }
+    val = val.replace(/\D/g, '');
+
+    if (this.GROUP_SEPARATOR === ',') {
+      return val.replace(/,/g, '');
+    } else {
+      return val.replace(/\./g, '');
+    }
   }
 
+  cpf_mask(v: string) {
+    v = v.replace(/\D/g, ''); //Remove tudo o que não é dígito
+    v = v.replace(/(\d{3})(\d)/, '$1.$2'); //Coloca um ponto entre o terceiro e o quarto dígitos
+    v = v.replace(/(\d{3})(\d)/, '$1.$2'); //Coloca um ponto entre o terceiro e o quarto dígitos
+    //de novo (para o segundo bloco de números)
+    v = v.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); //Coloca um hífen entre o terceiro e o quarto dígitos
+    return v;
+  }
+
+  cnpj(v: string) {
+    v = v.replace(/\D/g, ''); //Remove tudo o que não é dígito
+    v = v.replace(/^(\d{2})(\d)/, '$1.$2'); //Coloca ponto entre o segundo e o terceiro dígitos
+    v = v.replace(/^(\d{2})\.(\d{3})(\d)/, '$1.$2.$3'); //Coloca ponto entre o quinto e o sexto dígitos
+    v = v.replace(/\.(\d{3})(\d)/, '.$1/$2'); //Coloca uma barra entre o oitavo e o nono dígitos
+    v = v.replace(/(\d{4})(\d)/, '$1-$2'); //Coloca um hífen depois do bloco de quatro dígitos
+    return v;
+  }
 }
